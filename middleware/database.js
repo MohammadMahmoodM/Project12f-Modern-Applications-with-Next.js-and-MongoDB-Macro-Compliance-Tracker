e36171eba8c50f7e095226c5146127f6a4cc488f
mahmoodlegend <mohammadmahmood441@gmail.com>
2022-10-19 20:37:45 +0500

    now again trying to change URL to make it work online

--- middleware/database.js
+++ middleware/database.js
@@ -1,10 +1,13 @@
 import { MongoClient } from 'mongodb';
 import nextConnect from 'next-connect';
 
-const client = new MongoClient("mongodb://root:pak1437986@ac-vtgwnvm-shard-00-00.lss2kke.mongodb.net:27017,ac-vtgwnvm-shard-00-01.lss2kke.mongodb.net:27017,ac-vtgwnvm-shard-00-02.lss2kke.mongodb.net:27017/test?replicaSet=atlas-1469ge-shard-0&ssl=true&authSource=admin", {
+const client = new MongoClient("mongodb+srv://vercel-admin-user:tIhAqlqBcqbXToD0@cluster0.lss2kke.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+//locally
+
+// mongodb://root:pak1437986@ac-vtgwnvm-shard-00-00.lss2kke.mongodb.net:27017,ac-vtgwnvm-shard-00-01.lss2kke.mongodb.net:27017,ac-vtgwnvm-shard-00-02.lss2kke.mongodb.net:27017/test?replicaSet=atlas-1469ge-shard-0&ssl=true&authSource=admin  
 
 async function database(req, res, next) {
   if (!client.connect) await client.connect();
